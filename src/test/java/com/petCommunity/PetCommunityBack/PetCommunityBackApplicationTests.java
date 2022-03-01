61695e3da384927c8dc7240dea99c519b68ddc14
Exodya21 <jaumegomiciai@gmail.com>
2022-03-01 12:14:53 +0100

    first commit

---
+++ src/test/java/com/petCommunity/PetCommunityBack/PetCommunityBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petCommunity.PetCommunityBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCommunityBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
